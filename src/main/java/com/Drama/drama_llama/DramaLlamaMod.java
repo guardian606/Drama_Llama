cb4ef98e2aaa5c7443febc0908117e2cc04e9ab9
guardian606 <guardian_606@hotmail.com>
2025-02-20 20:05:28 -0500

    added the folders

---
+++ src/main/java/com/Drama/drama_llama/DramaLlamaMod.java
@@ -0,0 +1,10 @@
+package com.Drama.drama_llama;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("drama_llama")
+public class DramaLlamaMod {
+    public DramaLlamaMod() {
+        System.out.println("Drama Llama Mod has loaded!");
+    }
+}
