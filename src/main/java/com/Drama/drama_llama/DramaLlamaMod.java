d02fcc514393a84afdecde34d9da092ff739a95b
guardian606 <guardian_606@hotmail.com>
2025-02-21 10:12:28 -0500

    Update DramaLlamaMod.java

--- src/main/java/com/Drama/drama_llama/DramaLlamaMod.java
+++ src/main/java/com/Drama/drama_llama/DramaLlamaMod.java
@@ -1,10 +1,33 @@
 package com.Drama.drama_llama;
 
+import com.Drama.drama_llama.registry.ModBlocks;
+import com.Drama.drama_llama.registry.ModEntities;
+import com.Drama.drama_llama.registry.ModItems;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod("drama_llama")
+@Mod(DramaLlamaMod.MOD_ID)
 public class DramaLlamaMod {
+    public static final String MOD_ID = "dramallama";
+
     public DramaLlamaMod() {
-        System.out.println("Drama Llama Mod has loaded!");
+        var bus = FMLJavaModLoadingContext.get().getModEventBus();
+        
+        ModItems.register(bus);
+        ModBlocks.register(bus);
+        ModEntities.register(bus);
+        
+        bus.addListener(this::setup);
+        bus.addListener(this::clientSetup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // Common setup code
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        // Client-specific setup (like rendering entities)
     }
 }
